cd9baa795bea572d6f8b454dc65af6dd789e9c21
Mayusha04 <mayushachowdary95@gmail.com>
2021-12-28 17:22:46 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
+//function App() {
+  //return (
+    //<div className="App">
+      //<header className="App-header">
+      //  <img src={logo} className="App-logo" alt="logo" />
+        //<p>
+          //Edit <code>src/App.js</code> and save to reload.
+      //  </p>
+        //<a
+          //className="App-link"
+       //   href="https://reactjs.org"
+         // target="_blank"
+        //  rel="noopener noreferrer"
+     //   >
+     //     Learn React
+     //   </a>
+     // </header>
+    //</div>
+ // );
+//}
+
+//export default App;
+
+import React,{userState, useState} from "react";
+import "./App.css";
+export default function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> My count { count }</h1>
+      <button onClick={()=>setCount(count+1)}>ADD COUNT</button>
     </div>
   );
+    
 }
 
-export default App;
